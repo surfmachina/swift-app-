bd835d2c8ac5cf5373a116f523ac1ddf830695d9
Thomas Carlson <Tom@thomass-mac-pro.local>
2018-05-09 21:29:39 +1000

    Initial Commit

---
+++ SwiftAppt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftAppt
+//
+//  Created by Thomas Carlson on 9/5/18.
+//  Copyright © 2018 SurfMachina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
